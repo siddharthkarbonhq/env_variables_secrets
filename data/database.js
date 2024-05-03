cdb3d4f927fa612bbc32a31eeb1e231f33bc1ac4
Siddharth Sharma <siddharth.sharma@karbonhq.com>
2024-05-03 12:04:32 +1000

    databse.js update

--- data/database.js
+++ data/database.js
@@ -1,6 +1,32 @@
+// import { MongoClient } from 'mongodb';
+
+// const clusterAddress = process.env.MONGODB_ADDRESS;
+// const dbUser = process.env.MONGODB_USERNAME;
+// const dbPassword = process.env.MONGODB_PASSWORD;
+// const dbName = process.env.MONGODB_DB_NAME;
+
+// const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri);
+
+// console.log('Trying to connect to db');
+
+// try {
+//   await client.connect();
+//   await client.db(dbName).command({ ping: 1 });
+//   console.log('Connected successfully to server');
+// } catch (error) {
+//   console.log('Connection failed.');
+//   await client.close();
+//   console.log('Connection closed.');
+// }
+
+// const database = client.db(dbName);
+
+// export default database;
+
 import { MongoClient } from 'mongodb';
 
-const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
+const clusterAddress = process.env.MONGODB_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
@@ -10,15 +36,23 @@ const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
 
-try {
-  await client.connect();
-  await client.db(dbName).command({ ping: 1 });
-  console.log('Connected successfully to server');
-} catch (error) {
-  console.log('Connection failed.');
-  await client.close();
-  console.log('Connection closed.');
+async function main() {
+    try {
+        await client.connect();
+        await client.db(dbName).command({ ping: 1 });
+        console.log('Connected successfully to server');
+    } catch (error) {
+        console.error('Connection failed.', error);
+        try {
+            await client.close();
+            console.log('Connection closed.');
+        } catch (closeError) {
+            console.error('Failed to close the connection:', closeError);
+        }
+    }
 }
+
+main();
 
 const database = client.db(dbName);
 
